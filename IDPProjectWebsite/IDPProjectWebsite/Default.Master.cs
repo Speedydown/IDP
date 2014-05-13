d44eb352197d60ec83ba13a14b5ddc8eca5c10c7
Teake Otter <speedy@speedydown.nl>
2014-05-13 09:08:28 +0200

    MenuFix

--- IDPProjectWebsite/IDPProjectWebsite/Default.Master.cs
+++ IDPProjectWebsite/IDPProjectWebsite/Default.Master.cs
@@ -11,7 +11,60 @@ namespace IDPProjectWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            MenuDiv.Visible = true;
 
+            WebsiteMenuItem[] menuitems = {
+                                            new WebsiteMenuItem("/Default.aspx", "Thuisbasis", false),
+                                            new WebsiteMenuItem("/WeZijnWe.aspx", "Manage persons", false),
+                                            new WebsiteMenuItem("/WatDoenWe.aspx", "Manage TV shows", false),
+                                            new WebsiteMenuItem("/Contact.aspx", "Manage stats", false),
+                                       };
+
+            string url = Request.RawUrl.ToLower();
+
+            int startindex = 0;
+
+
+            int endindex = url.IndexOf(".aspx");
+            string page = "";
+
+            try
+            {
+                page = url.Substring(startindex, endindex - startindex + 5);
+            }
+            catch (Exception)
+            {
+                menuitems[0].Active = true;
+            }
+
+            foreach (WebsiteMenuItem w in menuitems)
+            {
+                if (w.ItemAdress.ToLower() == page.ToLower())
+                {
+                    w.Active = true;
+                }
+            }
+
+
+            MenuRepeater.DataSource = menuitems;
+            MenuRepeater.DataBind();
         }
     }
+
+
+}
+
+class WebsiteMenuItem
+{
+    public string ItemAdress { get; private set; }
+    public string ItemName { get; private set; }
+    public bool Active { get; set; }
+
+    public WebsiteMenuItem(string ItemAdress, string ItemName, bool Active)
+    {
+        this.ItemAdress = ItemAdress;
+        this.ItemName = ItemName;
+        this.Active = Active;
+    }
+
 }
--- IDPProjectWebsite/IDPProjectWebsite/Default.Master.designer.cs
+++ IDPProjectWebsite/IDPProjectWebsite/Default.Master.designer.cs
@@ -49,6 +49,15 @@ namespace IDPProjectWebsite {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl MenuDiv;
         
         /// <summary>
+        /// MenuRepeater control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater MenuRepeater;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
