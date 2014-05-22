0d5b9156769b15097d2f633c02e284997d7ce67e
Miriam <mairimoz@hotmail.com>
2014-05-22 15:33:10 +0200

    visionlabinterface

---
+++ VisionEngine/VisionEngine/VisionEngine/VisionLabInterface/VisionLabInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace VisionEngine.VisionLabInterface
+{
+    class VisionLabInterface
+    {
+        public VisionLabInterface() { }
+
+        public void processImage() { //Bitmap bmp
+            Bitmap bmpPicture = (Bitmap)Image.FromFile("image22.jpg");
+            JL_VisionLib_V3.CmdInt.Execute("icall findBalloonRed.jls");
+            JL_VisionLib_V3.CmdInt.SetImage("img", "RGB", bmpPicture);
+        }
+    }
+}
+
