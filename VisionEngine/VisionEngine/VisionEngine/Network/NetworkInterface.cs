2142a19e9f51f2139798029d821621925eb14866
Teake Otter <speedy@speedydown.nl>
2014-05-22 14:17:48 +0200

    netwerk update vision

--- VisionEngine/VisionEngine/VisionEngine/Network/NetworkInterface.cs
+++ VisionEngine/VisionEngine/VisionEngine/Network/NetworkInterface.cs
@@ -10,83 +10,33 @@ namespace VisionEngine.Network
 {
     class NetworkInterface
     {
-        private static StreamSocket socket;
-        private static HostName hostname;
-        private static DataReader reader;
-        private static DataWriter writer;
-        public static NetworkBuffer InputBuffer;
+        private NetworkBuffer _Buffer;
+        private TcpClient  clientSocket = new TcpClient();
+        private NetworkStream serverStream;
 
-
-        public static async Task Connect(string Hostname)
+        public NetworkInterface()
         {
-            socket = new StreamSocket();
-            hostname = new HostName(Hostname);
-            InputBuffer = new NetworkBuffer();
-
-
-            try
-            {
-                await socket.ConnectAsync(hostname, "1337");
-            }
-            catch (Exception e)
-            {
-                switch (SocketError.GetStatus(e.HResult))
-                {
-                    case SocketErrorStatus.HostNotFound:
-                        break;
-                    case SocketErrorStatus.UnreachableHost:
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-
-            // add after calling ConnectAsync on the StreamSocket Object
-            reader = new DataReader(socket.InputStream);
-            writer = new DataWriter(socket.OutputStream);
-            InputBuffer.Append("Connected to: " + hostname + "!");
+            serverStream = clientSocket.GetStream();
         }
 
-
-        public static async Task Recv()
+        public void Send(string Data)
         {
-            // container for the received Data
-            string receivedData;
-            reader.InputStreamOptions = InputStreamOptions.Partial;
-            var count = await reader.LoadAsync(4096);
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(Data);
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
 
-
-            // read the data as a string and store it in our container
-            if (count > 0)
-            {
-                receivedData = reader.ReadString(count);
-                InputBuffer.Append(receivedData);
-            }
         }
 
-
-        public static async Task Send(string Data)
+        public void Recv()
         {
-            // write a string to the OutputStream
-            if (Data.Length > 0)
-            {
-                Data += "<EOF>";
-                writer.WriteString(Data);
-                // commit and send the data in the OutputStream
-                await writer.StoreAsync();
-            }
-        }
+            byte[] inStream = new byte[1024];
+            
+            string Data = string.Empty;
+            while (Data.Contains("<EOF>"))
 
-
-        public static async Task Close()
-        {
-            await (Send("exit"));
-            if (socket == null)
-            {
-                socket.Dispose();
-            }
-            App.Current.Exit();
+            serverStream.Read(inStream, 0, (int)clientSocket.ReceiveBufferSize);
+            string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+            
         }
 
     }
