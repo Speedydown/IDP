9845a584e745bc8ff7c7f500959925ae71c2d65c
Matthe Jacobs <m_jacobs@hotmail.nl>
2014-05-23 13:25:24 +0200

    Standard Setup
    Hello

--- IDPSpin/MotionInterface.py
+++ IDPSpin/MotionInterface.py
@@ -5,7 +5,7 @@ from Leg import Leg
 class MotionInterface(object):
 
     def __init__(self):
-        self.Leg1 = Leg(["0x40", "0x40", "0x40"], [0, 1, 2], [375, 375, 375])
+        self.Leg1 = Leg(["0x40", "0x40", "0x40"], [0, 1, 2], [600, 350, 375])
 
     def test(self, Pulse):
         print "Pulse: " + Pulse
