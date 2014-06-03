16c62fbb2217506da807422f176a2867bb96fb69
Ivar <ivardelelange2003@hotmail.com>
2014-06-03 12:33:47 +0200

    Comments changed

--- IDPSpin/Leg.py
+++ IDPSpin/Leg.py
@@ -4,18 +4,16 @@ from Servo import Servo
 
 class Leg(object):
     """
-    All 6 legs will be added to an array. All the address and channels of the specific leg will be added
-    into an array. The starting position of the spider is put into an array.
-    The leg is defined in 3 groups: Hip, Knee and Ankle.
+    Setting the addresses, channels and default position of the 3 servos of the leg.
     """
-
 
     def __init__(self, AddressArray, ChannelArray, DefaultPulseArray):
         self._AddressArray = AddressArray
         self._ChannelArray = ChannelArray
         self._DefaultPulseArray = DefaultPulseArray
 
-    # Make address on for 3 servos. First Hip then Knee and last Ankle
+    # Receiving variables, this will be set into arrays. If there aren't 3 input variables of the address
+    # it will print an Error.
         if len(self._AddressArray) == 3:
             self._Hip = Servo(self._AddressArray[0], self._ChannelArray[0], self._DefaultPulseArray[0])
             self._Knee = Servo(self._AddressArray[1], self._ChannelArray[1], self._DefaultPulseArray[1])
@@ -23,19 +21,19 @@ class Leg(object):
         else:
             print "-----------Error Creating leg-----------"
 
-    # Moving the Hip of the Leg
+    # Setting the Hip to the position of the given pulse
     def moveHip(self, Pulse):
         self._Hip.setPulse(Pulse)
 
-    # Moving the Knee of the Leg
+    # Setting the Knee to the position of the given pulse
     def moveKnee(self, Pulse):
         self._Knee.setPulse(Pulse)
 
-    # Moving the Ankle of the Leg
+    # Setting the Ankle to the position of the given pulse
     def moveAnkle(self, Pulse):
         self._Ankle.setPulse(Pulse)
 
-    # Moving the full leg to starting position. Pulse will be set on 375
+    # Setting the full Leg to the starting position of 375 pulses
     def defaultPos(self):
         self.moveHip(self._Hip, self._DefaultPulseArray[0])
         self.moveKnee(self._Knee, self._DefaultPulseArray[1])
--- IDPSpin/Servo.py
+++ IDPSpin/Servo.py
@@ -4,31 +4,32 @@ from Adafruit_PWM_Servo_Driver import PWM
 
 class Servo(object):
     """
-    Setting the channels, address, startingPulse and frequency. Using the Adafruit PWM servo driver to
-    set the min and max Pulse of the servo for maximum degrees possible.
+    Setting the channels, address, startingPulse and frequency to create an servo.
+    Using the Adafruit PWM servo driver to set the min and max Pulse of the servo.
     """
     def __init__(self, address, channel, startingPulse, minPulse=150, maxPulse=600, freq=60):
-
-        self.pulse = 0                      # set pulse on 0
-        self.channel = channel              # channels of the adafruit servo driver (0 to 16) used 2times
-        self.address = address              # 2 addresses of the adafruit (0x40,0x41)
-        self.freq = freq                    # standard frequency for the adafruit (60Hz)
-        self.maxPulse = maxPulse            # setting the maximum pulse of the servo
-        self.minPulse = minPulse            # setting the minimum pulse of the servo
+    # Constructor
+        self.pulse = 0
+        self.channel = channel
+        self.address = address
+        self.freq = freq
+        self.maxPulse = maxPulse
+        self.minPulse = minPulse
         self.pwm = PWM()
         self.pwm.setPWMFreq(self.freq)
         self.setPulse(startingPulse)
 
     def setPulse(self, pulse):
-        # If the input pulse is bigger then maxPulse, then set to maxPulse,
-        # for input smaller then minPulse, then set to minPulse.
+        # Setting the area of pulses between 150 and 600. If the input is bigger then 600 pulses
+        # it will be set to the max 600 pulses. If pulses are lower then 150 it will be set tot 150 pulses
+
         if pulse > self.maxPulse:
             pulse = self.maxPulse
         elif pulse < self.minPulse:
             pulse = self.minPulse
 
         self.pwm.setPWM(self.channel, 0, pulse) # 0 means: zero pulse away from input pulse.
-        self.pulse = pulse
+        self.pulse = pulse                      # setting the input pulse
 
     def getPulse(self):
         return self.pulse
