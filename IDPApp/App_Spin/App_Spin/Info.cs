fc5cdcbca922cd43c69a3364ee0db3f79c53754d
reversj <nielsriemersma@hotmail.com>
2014-06-15 19:03:46 +0200

    app mainpage edit
    mainpage = klaar

---
+++ IDPApp/App_Spin/App_Spin/Info.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Spin
+{
+    public class Info
+    {
+        private static string connState;
+        private static bool connected;
+
+        public void setState(string state)
+        {
+            connState = state;
+        }
+
+        public string getState()
+        {
+            return connState;
+        }
+
+        public void setConnected(bool conn)
+        {
+            connected = conn;
+        }
+
+        public bool getConnected()
+        {
+            return connected;
+        }
+    }
+}
