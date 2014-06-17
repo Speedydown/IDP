f6d33bb17ff8611f50f139f15cea411946f52d4f
Ivar <ivardelelange2003@hotmail.com>
2014-06-17 09:40:48 +0200

    Fix
    commit moet ff

--- IDPSpin/SpiderGap.py
+++ IDPSpin/SpiderGap.py
@@ -11,9 +11,9 @@ class GapMove(object):
 
     def __init__(self, _MInterface):
         self._MInterface = _MInterface
-        self.group1 = [self._MInterface._Legs[1], self._MInterface._Legs[1]]
+        self.group1 = [self._MInterface._Legs[0], self._MInterface._Legs[1]]#achterpoten
         self.group2 = [self._MInterface._Legs[2], self._MInterface._Legs[3]]
-        self.group3 = [self._MInterface._Legs[4], self._MInterface._Legs[5]]
+        self.group3 = [self._MInterface._Legs[4], self._MInterface._Legs[5]]#voorpoten
 
     def executeCommand(self, Command):
         Command = int(Command)
@@ -24,11 +24,15 @@ class GapMove(object):
     def exit(self):
         pass #exit methode
 
-    def PullLegsForward(self, Legs):
-        
-
+    def PullLegsForward(self):
+        group1.moveAnkle(150), group1.moveKnee(150), group1.moveHip(150)
+        group2.moveAnkle(550), group2.moveKnee(550), group2.moveHip(375)
+        group3.moveAnkle(600), group3.moveKnee(600), group3.moveHip(600)
 
     def PullForward(self):
+
+
+
 
 
 
