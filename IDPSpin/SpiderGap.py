36a607b4949888f19be4de02e903cef3985f1c4e
Ivar <ivardelelange2003@hotmail.com>
2014-06-17 13:53:14 +0200

    SPiderGapCommit

--- IDPSpin/SpiderGap.py
+++ IDPSpin/SpiderGap.py
@@ -18,18 +18,50 @@ class GapMove(object):
     def executeCommand(self, Command):
         Command = int(Command)
 
-        if Command == 99:
+        if Command == 3:
             self.pullForward()
 
     def exit(self):
         pass #exit methode
 
     def PullLegsForward(self):
-        group1.moveAnkle(150), group1.moveKnee(150), group1.moveHip(150)
-        group2.moveAnkle(550), group2.moveKnee(550), group2.moveHip(375)
-        group3.moveAnkle(600), group3.moveKnee(600), group3.moveHip(600)
+        group1.moveHip(550), group1.moveKnee(200), group1.moveAnkle(550)
+        group2.moveHip(375), group2.moveKnee(400), group2.moveAnkle(550)
+        group3.moveHip(200), group3.moveKnee(550), group3.moveAnkle(550)
 
-    def PullForward(self):
+        group1.moveHip(550), group1.moveKnee(250), group1.moveAnkle(550)
+        group2.moveHip(375), group2.moveKnee(400), group2.moveAnkle(550)
+        group3.moveHip(550), group3.moveKnee(200), group3.moveAnkle(550)
+
+
+    def PullForward(self, Step, numberSteps=160):
+        if Step > numberSteps or Step < 1:
+            print "Step out of range - " + str(Step)
+            raise Exception("")
+
+        leg1Thread = threading.Thread(target=self._MInterface.PullLegsForward, args = (self.group1))
+        leg1Thread.start()
+        leg2Thread = threading.Thread(target=self._MInterface.PullLegsForward, args = (self.group2))
+        leg2Thread.start()
+        leg3Thread = threading.Thread(target=self._MInterface.PullLegsForward, args = (self.group3))
+        leg3Thread.start()
+
+        leg1Thread.join()
+        leg2Thread.join()
+        leg3Thread.join()
+
+
+        group1.moveHip(550),group1.moveKnee(200),group1.moveAnkle(550)
+        group2.moveHip(375),group2.moveKnee(400),group2.moveAnkle(550)
+        group3.moveHip(200),group3.moveKnee(550),group3.moveAnkle(550)
+
+        group1.moveHip(550),group1.moveKnee(250),group1.moveAnkle(550)
+        group2.moveHip(375),group2.moveKnee(400),group2.moveAnkle(550)
+        group3.moveHip(550),group3.moveKnee(200),group3.moveAnkle(550)
+
+
+
+
 
 
 
